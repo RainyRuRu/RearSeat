2013d9a824e7c3860987474c3ba05c483fd5779e
RainyRuRu <believeu123@gmail.com>
2016-06-20 00:11:06 +0800

    message model

---
+++ model/MessageModel.php
@@ -0,0 +1,15 @@
+<?php
+namespace RearSeat;
+
+class MessageModel
+{
+    public static function searchMessages($id)
+    {
+
+    }
+
+    public static function insertMessages($id, $message, $user)
+    {
+
+    }
+}
