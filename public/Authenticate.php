7f6629ada9eb178af2bdc96b95d2f649448176d5
RainyRuRu <believeu123@gmail.com>
2016-06-19 14:46:10 +0800

    authenticate result

--- public/Authenticate.php
+++ public/Authenticate.php
@@ -10,10 +10,10 @@ $twig = new Twig_Environment($loader);
 $email = $_GET['mail'];
 $code = $_GET['code'];
 
-UserModel::authenticate($email, $code);
+$result = UserModel::authenticate($email, $code);
 
-var_dump($email);
-var_dump($code);
-var_dump('hi');
-
-//echo $twig->render('test.twig.html', $param);
+if ($result) {
+    //echo $twig->render('test.twig.html', $param);
+} else {
+    
+}
