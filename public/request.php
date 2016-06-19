7848903095403b41afdd97547a8c486d80e2e117
snoopy2199 <snoopy2199@gmail.com>
2016-06-20 05:52:16 +0800

    request

---
+++ public/request.php
@@ -0,0 +1,34 @@
+<?php
+
+include __DIR__ . '/../vendor/autoload.php';
+
+use RearSeat\SeatModel;
+use RearSeat\UserModel;
+use RearSeat\UserSession;
+
+session_start();
+
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/../views/');
+$twig = new Twig_Environment($loader);
+
+$result = SeatModel::searchSeatByRequest(1);
+
+$data = [];
+
+foreach ($result as $seat) {
+    $user = UserModel::searchUser($seat['reporter']);
+    $seat['photo'] = base64_encode($user['photo']);
+    $seat['name'] = $user['name'];
+    array_push($data, $seat);
+}
+
+$param = [
+    "user" => [
+        'id' => UserSession::getUserId(),
+        'photo' => UserSession::getUserPhoto(),
+    ],
+    "data" => $data,
+    "page" => "share"
+];
+
+echo $twig->render('request.html', $param);
