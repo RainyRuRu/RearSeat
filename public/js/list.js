ea5163f4bcea3bdb7a1123263b3aab987126472f
snoopy2199 <snoopy2199@gmail.com>
2016-06-20 04:18:15 +0800

    list page

---
+++ public/js/list.js
@@ -0,0 +1,25 @@
+ jQuery(document).ready(function($) {
+    $(".clickable-row").click(function() {
+        window.document.location = $(this).data("href");
+    });
+});
+
+ function onload(){
+    $('#pagination').twbsPagination({
+        totalPages: 1,
+        visiblePages: 5,
+        onPageClick: function (event, page) {}
+    });
+ }
+
+ function selectOption(option) {
+    document.getElementById("select_option").innerHTML = option;
+}
+
+function goSearch() {
+    var keyword = document.forms["search_form"]["keyword"].value;
+    var request = document.getElementById("select_option").innerHTML == "找車位" ? 0 : 1;
+    window.location.assign("search.php?keyword=" + keyword + "&request=" + request);
+    return false;
+}
+    
