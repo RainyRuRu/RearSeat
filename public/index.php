3d2eb050152f89304caf72e18051e448ce173c0b
RainyRuRu <believeu123@gmail.com>
2016-06-19 16:55:08 +0800

    login

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ use RearSeat\SeatModel;
 use RearSeat\UserModel;
 use RearSeat\UserSession;
 
+session_start();
+
 $loader = new Twig_Loader_Filesystem(__DIR__ . '/../views/');
 $twig = new Twig_Environment($loader);
 
